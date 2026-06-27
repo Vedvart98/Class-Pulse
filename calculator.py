006b3d31ddda5d27d19e7a4f53620e41bef6d6c8
Vedvart98 <yadavvedvart@gmail.com>
2026-06-27 23:30:45 +0530

    git merge request from junior a

--- calculator.py
+++ calculator.py
@@ -2,4 +2,4 @@ def add(a, b):
     return a + b
 
 def greet():
-    print("Hello from Branch A - feature complete and learning git conflcts!")
+    print("Hello from Branch A - feature complete and learning git conflcts with junior A")
